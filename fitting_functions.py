6dd43563cbec5aff77caadc8952bac844215e370
Jordi <reddepth825@gmail.com>
2025-11-17 14:51:36 -0500

    Poject 2 update

--- fitting_functions.py
+++ fitting_functions.py
@@ -3,12 +3,12 @@ def linear(m, x, b):
 
 
 def slope_units(x_units, y_units):
-    x_clean = x_units.rstrip('s')
-    y_clean = y_units.rstrip('s')
+    x_clean = x_units.rstrip("s")
+    y_clean = y_units.rstrip("s")
+
     return f"{y_clean}/{x_clean}"
 
 
-def print_equation(m, b, x_units, y_units):
-    slope = slope_units(x_units, y_units)
-    print(f"The equation of the line is: y = {m} {slope} x + {b} {y_units.rstrip('s')}")
-
+def print_equation(m, b, y_units, x_units):
+    units = slope_units(x_units, y_units)
+    print(f"The equation of the line is: y =  {m} {units} x + {b} {y_units.rstrip('s')}")
--- test_fnx.py
+++ test_fnx.py
@@ -1,23 +1,16 @@
-# test_fxn.py
 from fitting_functions import *
 
-# Test 1
-print("Test 1: linear(2, 3, 4)")
-print("Expected:", 10)
-print("Got:", linear(2, 3, 4))
+#Test1
+print("Testing linear(m, x, b):")
+print("Expected 10 →", linear(2, 3, 4))
 print()
 
-# Test 2
-print("Test 2: slope_units('meters', 'kg')")
-print("Expected:", "kg/meter")
-print("Got:", slope_units('meters', 'kg'))
+#Test2
+print("Testing slope_units(x_units, y_units):")
+print("meters, kg →", slope_units("meters", "kg"))   
+print("sec, meters →", slope_units("sec", "meters")) 
 print()
 
-print("Test 3: slope_units('sec', 'meters')")
-print("Expected:", "meter/sec")
-print("Got:", slope_units('sec', 'meters'))
-print()
-
-# Test 3
-print("Test 4: print_equation(4, 1, 'meters', 'kg')")
-print_equation(4, 1, 'meters', 'kg')
+#Test2
+print("Testing print_equation():")
+print_equation(4, 1, "kg", "meters")
